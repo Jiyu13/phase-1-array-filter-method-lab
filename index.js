129531d978752255f1500389787be24968849d6b
Ziru Zou <jiyucoding@yahoo.com>
2023-01-14 23:16:27 -0500

    returns all drivers whose names begin with the provided letters

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function findMatching(arr, str) {
     const matchDrivers = arr.filter(driver => driver.toUpperCase() === str.toUpperCase());
     return matchDrivers;
 } 
+
+
+function fuzzyMatch (arr, str) {
+    const matchDrivers = arr.filter(driver => driver.startsWith(str));
+    return matchDrivers;
+}
