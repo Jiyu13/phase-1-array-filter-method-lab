98b7a70b8d92ff8897dac8a80e0bd3912e010f7f
Ziru Zou <jiyucoding@yahoo.com>
2023-01-14 23:18:54 -0500

    return each element whose name property matches the provided str argument

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ function fuzzyMatch (arr, str) {
     const matchDrivers = arr.filter(driver => driver.startsWith(str));
     return matchDrivers;
 }
+
+function matchName(arr, str) {
+    const matchDriver = arr.filter(driver => driver.name === str)
+    return matchDriver;
+}
